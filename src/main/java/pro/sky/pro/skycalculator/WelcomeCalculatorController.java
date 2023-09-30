a813a2ff01688c8b02a249549f5802d23ab1e82e
vlj2007 <vlj2007@yandex.ru>
2023-10-01 02:10:22 +0700

    I have added new code in files

--- src/main/java/pro/sky/pro/skycalculator/WelcomeCalculatorController.java
+++ src/main/java/pro/sky/pro/skycalculator/WelcomeCalculatorController.java
@@ -1,6 +1,7 @@
 package pro.sky.pro.skycalculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +22,30 @@ public class WelcomeCalculatorController {
         return "<center><b> Добро пожаловать в калькулятор </b></center>";
     }
 
+    @GetMapping(path = "/calculator")
+    public String answerWelcome(@RequestParam("name") String userName) {
+        return welcomeCalculatorService.answerWelcome(userName);
+    }
+
+    @GetMapping(path = "/calculator/plus")
+    public String plusWelcome(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return welcomeCalculatorService.plusWelcome(num1, num2);
+    }
+
+    @GetMapping(path = "/calculator/minus")
+    public String minusWelcome(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return welcomeCalculatorService.minusWelcome(num1, num2);
+    }
+
+    @GetMapping(path = "/calculator/multiply")
+    public String multiplyWelcome(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return welcomeCalculatorService.multiplyWelcome(num1, num2);
+    }
+
+    @GetMapping(path = "/calculator/divide")
+    public String divideWelcome(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return welcomeCalculatorService.divideWelcome(num1, num2);
+
+    }
 
 }
--- src/main/java/pro/sky/pro/skycalculator/WelcomeCalculatorService.java
+++ src/main/java/pro/sky/pro/skycalculator/WelcomeCalculatorService.java
@@ -1,4 +1,44 @@
 package pro.sky.pro.skycalculator;
 
-public class WelcomeCalculatorService {
+import org.springframework.stereotype.Service;
+
+@Service
+public class WelcomeCalculatorService implements CalculatorInterface{
+
+    public String plusWelcome(int... values) {
+        int num1 = Integer.parseInt(String.valueOf(values[0]));
+        int num2 = Integer.parseInt(String.valueOf(values[1]));
+        int Result = num1 + num2;
+        return "<center><b>" + num1 + " + " + num2 + " =  " + Result + "</center></b>";
+
+    }
+
+    public String minusWelcome(int... values) {
+        int num1 = Integer.parseInt(String.valueOf(values[0]));
+        int num2 = Integer.parseInt(String.valueOf(values[1]));
+        int Result = num1 - num2;
+        return "<center><b>" + num1 + " - " + num2 + " =  " + Result + "</center></b>";
+    }
+
+    @Override
+    public String answerWelcome(String userName) {
+        return "<center><b> Добро пожаловать в калькулятор " + userName + "</b></center>";
+    }
+
+    public String multiplyWelcome(int... values) {
+        int num1 = Integer.parseInt(String.valueOf(values[0]));
+        int num2 = Integer.parseInt(String.valueOf(values[1]));
+        int Result = num1 * num2;
+        return "<center><b>" + num1 + " * " + num2 + " =  " + Result + "</center></b>";
+    }
+
+    public String divideWelcome(int num1, int num2) {
+        if (num2 == 0) {
+            return "Деление на 0 недопустимо";
+        } else {
+            return "<center><b>" + num1 + " / " + num2 + " = " + String.valueOf((float) (num1 / num2)) + "</center></b>";
+        }
+
+    }
 }
+
